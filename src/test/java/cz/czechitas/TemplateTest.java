5d9a19f0379f14c1cf5fdb7f5aa866c8cb298516
Monika Protivová <monika.protivova@cleverfarm.cz>
2020-11-14 09:21:40 +0100

    baseline

---
+++ src/test/java/cz/czechitas/TemplateTest.java
@@ -0,0 +1,25 @@
+package cz.czechitas;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TemplateTest {
+
+
+    @BeforeEach
+    public void before() {
+
+    }
+
+    @AfterEach
+    public void after() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
