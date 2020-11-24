6cb3bb8c7f0533ca40079d90acb6b0802bdbbaaf
Lenka Cimlerova <lenka.cimlerova@email.cz>
2020-11-24 18:36:48 +0100

    verze3

--- src/test/java/cz/czechitas/TemplateTest.java
+++ src/test/java/cz/czechitas/TemplateTest.java
@@ -1,25 +1,49 @@
 package cz.czechitas;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 
 public class TemplateTest {
 
+    private static WebDriver driver;
+    private static AdminPage adminLogonUrl;
 
     @BeforeEach
     public void before() {
+        driver = DriverHelper.openFirefox();
+        adminLogonUrl = new AdminPage (driver);
+        adminLogonUrl.openPage();
 
     }
 
     @AfterEach
     public void after() {
-
+        driver.quit();
     }
 
     @Test
-    public void test() {
+    @DisplayName("Login failed - wrong password")
+    public void testWrongPassword() {
 
+        adminLogonUrl.ValidUsernameLogin();
+        adminLogonUrl.InvalidPasswordLogin();
+        adminLogonUrl.ConfirmLogin();
+    }
+
+    @Test
+    @DisplayName ("Login failed, wrong username")
+    public void testWrongUsername() {
+        adminLogonUrl.InvalidUsernameLogin();
+        adminLogonUrl.ValidPasswordLogin();
+        adminLogonUrl.ConfirmLogin();
+    }
+
+    @Test
+    @DisplayName ("Successful login")
+    public void testSuccessfulLogin() {
+        adminLogonUrl.ValidUsernameLogin();
+        adminLogonUrl.ValidPasswordLogin();
+        adminLogonUrl.ConfirmLogin();
     }
 
 }
