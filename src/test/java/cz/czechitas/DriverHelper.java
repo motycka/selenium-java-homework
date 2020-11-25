9edaf1494d6e4ea7ad898c6ef27912834e781a9c
Monika Protivová <monika.protivova@cleverfarm.cz>
2020-11-25 20:28:29 +0100

    WebDriverManager

--- src/test/java/cz/czechitas/DriverHelper.java
+++ src/test/java/cz/czechitas/DriverHelper.java
@@ -1,5 +1,6 @@
 package cz.czechitas;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -16,7 +17,7 @@ public class DriverHelper {
     private static String downloadsPath = "";
 
     public static WebDriver openFirefox() {
-        setDriver("gecko");
+        WebDriverManager.firefoxdriver().setup();
         FirefoxOptions options = new FirefoxOptions()
                 .addArguments("-private")
                 .addPreference("browser.download.dir", getPath(downloadsPath))
@@ -27,7 +28,7 @@ public class DriverHelper {
     }
 
     public static WebDriver openChrome() {
-        setDriver("chrome");
+        WebDriverManager.chromedriver().setup();
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("download.default_directory", getPath(downloadsPath));
         ChromeOptions options = new ChromeOptions()
@@ -36,19 +37,6 @@ public class DriverHelper {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         return driver;
-    }
-
-    public static void setDriver(String driver) {
-        String os = System.getProperty("os.name").toLowerCase();
-
-        String driverPath = "";
-        if (os.contains("win")) {
-            driverPath = "drivers/" + driver + "driver.exe";
-        } else {
-            driverPath = "drivers/" + driver + "driver";
-        }
-
-        System.setProperty("webdriver." + driver + ".driver", getPath(driverPath));
     }
 
     private static String getPath(String path) {
